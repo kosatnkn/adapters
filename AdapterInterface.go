200b4134a0968b6cbd6b4559ded0d1d29b9cb9a4
Kosala Tennakoon <kosala.me@gmail.com>
2021-02-22 19:50:36 +0530

    added QueryBulk

--- AdapterInterface.go
+++ AdapterInterface.go
@@ -12,11 +12,16 @@ type AdapterInterface interface {
 	Ping() error
 
 	// Query runs a query and return the result.
-	Query(ctx context.Context, query string, parameters map[string]interface{}) ([]map[string]interface{}, error)
+	Query(ctx context.Context, query string, params map[string]interface{}) ([]map[string]interface{}, error)
+
+	// QueryBulk runs a query using an array of parameters.
+	//
+	// This query is intended to do bulk inserts, updates and deletes. Using this for selects will result in an error.
+	QueryBulk(ctx context.Context, query string, params []map[string]interface{}) ([]map[string]interface{}, error)
 
 	// NewTransaction creates a new database transaction.
 	NewTransaction() (*sql.Tx, error)
 
 	// Destruct will close the database adapter releasing all resources.
-	Destruct()
+	Destruct() error
 }
